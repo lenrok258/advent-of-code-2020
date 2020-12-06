851b972c9218f933176924eaf62a14b2178770eb
Kornel Zemla <kornelzemla@gmail.com>
2020-12-06 21:17:42 +0100

    Making solution more concise

--- 006/script.py
+++ 006/script.py
@@ -1,28 +1,18 @@
-import string
+from string import ascii_lowercase
 
-input_file = open('input.txt', 'r')
-lines = input_file.readlines()
-
-
-def full_group():
-    return list(string.ascii_lowercase)
-
+lines = open('input.txt', 'r').read().splitlines()
 
 count = 0
-prv_group = full_group()
-current_group = list()
+prv_group = set(ascii_lowercase)
 for line in lines:
-    line = line.replace('\n', '')
 
-    if len(line) == 0:
-        current_group = list(set(current_group))
-        count += len(current_group)
-        prv_group = full_group()
-        current_group.clear()
+    if not len(line):
+        count += len(prv_group)
+        prv_group = set(ascii_lowercase)
         continue
 
-    line_chars = list(line)
-    current_group = list(set(prv_group).intersection(line_chars))
-    prv_group = current_group
+    prv_group = prv_group.intersection(line)
 
-print("\n░░░░░░░░░░░░░░░░░░░\n░░░ Answer {} ░░░\n░░░░░░░░░░░░░░░░░░░".format(count))
+print(count)
+
+

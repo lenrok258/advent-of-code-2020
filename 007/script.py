4ed1685e2aea64cc662a9181a6350ce17db68c4d
kornel.zemla <kornel.zemla@ocado.com>
2020-12-07 10:45:59 +0100

    Day 7, star 2

--- 007/script.py
+++ 007/script.py
@@ -12,11 +12,23 @@ def count_outer_bags(color):
             outer_colors.update(count_outer_bags(outer))
     return outer_colors
 
+def count_inner_bags(color):
+    lines = open('input.txt', 'r').read().splitlines()
+    counter = 0 
+    for line in lines:
+        outer_inner = line.split(" bags contain ")
+        outer = outer_inner[0]
+        inner = outer_inner[1].replace('.', '').replace(' bags', '').replace(' bag', '')
 
-outer_colors = count_outer_bags("shiny gold")
+        if color in outer:
+            if "no other bags" in line:
+                return 0
+            inners = inner.split(", ")
+            for i in inners:
+                i_array = i.split(" ", 1)
+                counter += int(i_array[0])
+                counter += int(i_array[0]) * count_inner_bags(i_array[1])
+    return counter
 
-print(outer_colors)
-
-print(len(outer_colors))
-
-
+print(len(count_outer_bags("shiny gold")))
+print(count_inner_bags("shiny gold"))

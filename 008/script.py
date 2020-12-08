6129c719c666698a63f8f7fe7293afcfd947f0d5
kornel.zemla <kornel.zemla@ocado.com>
2020-12-08 10:42:03 +0100

    Day 8, star 2

--- 008/script.py
+++ 008/script.py
@@ -1,27 +1,53 @@
+
+def parse_line(line):
+    tiles = line.split(" ")
+    return (tiles[0], int(tiles[1]))
+
+def execute_program(code_to_test):
+    idx = acc = 0
+    while True:
+        if idx >= len(code_to_test):
+            return ('BOOTED', acc)
+
+        line = code_to_test[idx]
+        code_to_test[idx] = 'INF_LOOP'
+        
+        if 'INF_LOOP' in line:
+            return ('INF_LOOP', acc)
+
+        code, value = parse_line(line)
+        print(f"{code}:{value}\t\tacc:{acc}")
+
+        if code == 'nop':
+            idx += 1
+        elif code == 'acc':
+            idx += 1
+            acc += value
+        elif code == 'jmp':
+            idx += value
+
+
 lines = open('input.txt', 'r').read().splitlines()
 
-idx = 0
-acc = 0
-while True:
-    line = lines[idx]
+# star 1
+result = execute_program(lines.copy())
+print(f"\n░░░ {result} ░░░\n")
+
+# star 2
+for i in range(len(lines)):
+    code_mutation = lines.copy()
+    candidate = code_mutation[i]
+
+    if 'jmp' in candidate:
+        code_mutation[i] = candidate.replace('jmp', 'nop')
+    elif 'nop' in candidate:
+        code_mutation[i] = candidate.replace('nop', 'jmp')
+    else:
+        continue
+
+    exit_code, acc = execute_program(code_mutation)
+    print(f"\n░░░ {exit_code}, acc:{acc} ░░░\n")
     
-    if 'TERM' in line:
+    if exit_code == 'BOOTED':
         break
-
-    line_array = line.split(" ")
-    code = line_array[0]
-    value = int(line_array[1])
-    print(f"{line_array} acc:{acc}")
-
-    if code == 'nop':
-        lines[idx] = 'TERM'
-        idx += 1
-    elif code == 'acc':
-        lines[idx] = 'TERM'
-        idx += 1
-        acc += value
-    elif code == 'jmp':
-        lines[idx] = 'TERM'
-        idx += value
-
-print("Answer 1: {}".format(acc))
+    

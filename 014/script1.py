aa2c60325f4f769f02f74d45e55112a0b4ae8c24
kornel.zemla <kornel.zemla@ocado.com>
2020-12-14 12:28:10 +0100

    Day 14, star 1

--- 014/script1.py
+++ 014/script1.py
@@ -1,9 +1,47 @@
 import itertools
 import cProfile
+import functools
 
 # test: 165
-# input: 
+# input: 15403588588538
 
-lines = open('input_test1.txt', 'r').read().splitlines()
 
-print(lines)
+def to_binary_string(intput_int):
+    return format(intput_int, '#036b')
+
+
+def parse_input_line(line):
+    tiles = line.split(' = ')
+    if tiles[0] == 'mask':
+        return tiles
+    else:
+        mem_addr = tiles[0].split("[")[1].split("]")[0]
+        return (int(mem_addr), int(tiles[1]))
+
+
+def apply_mask(mask, value):
+    and_mask = mask.replace('X', '1')
+    or_mask = mask.replace('X', '0')
+    return value & int(and_mask, 2) | int(or_mask, 2)
+
+
+def initialize_program(commands):
+
+    memory = dict()
+    curr_mask = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+
+    for c in commands:
+        instr, value = c
+        if instr == 'mask':
+            curr_mask = value
+        else:
+            memory[address] = apply_mask(mask, value)
+
+    return memory
+
+
+commands = map(parse_input_line, open('input.txt', 'r').read().splitlines())
+
+memory = initialize_program(commands)
+
+print(sum(memory.values()))
